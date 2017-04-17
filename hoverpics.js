a4e3e68c7f592fb0679df2a9e6269d24b6e9dc4e
tylertucker55 <tylertucker55@gmail.com>
2017-04-16 23:07:39 -0600

    tried to add shine.js

---
+++ hoverpics.js
@@ -0,0 +1,38 @@
+ $(document).ready(function() {
+    $('.rollme').mouseenter(function() {
+        $('.rollme').fadeTo('fast', 0.5);
+        $('.captionme').fadeTo('fast', 1);
+    });
+    
+    $('.rollme').mouseleave(function() {
+        $('.rollme').fadeTo('fast', 1);
+        $('.captionme').fadeTo('fast', 0);
+});
+               
+       $('.bannerhome').click(function() {
+        $('.bannerhome').hide('slow');
+           
+});
+            
+        
+});
+            
+            $(document).ready(function() {
+    $('.rollme2').mouseenter(function() {
+        $('.rollme2').fadeTo('fast', 0.5);
+        $('.captionme2').fadeTo('fast', 1);
+    });
+    
+    $('.rollme2').mouseleave(function() {
+        $('.rollme2').fadeTo('fast', 1);
+        $('.captionme2').fadeTo('fast', 0);
+});
+               
+       $('.bannerhome2').click(function() {
+        $('.bannerhome2').hide('slow');
+           
+});
+            
+        
+});
+    
