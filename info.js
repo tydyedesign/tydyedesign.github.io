9ebaa4efffd5ad49b36f1d50212d108a30130c6c
tylertucker55 <tylertucker55@gmail.com>
2019-03-16 15:13:39 -0600

    updated info checker
    also hid night mode

---
+++ info.js
@@ -0,0 +1,7 @@
+document.getElementById("appversion").innerHTML = navigator.appVersion;
+
+document.getElementById("osversion").innerHTML = 
+"OS Version: " + navigator.platform;
+
+document.getElementById("cookiecheck").innerHTML =
+"Cookies: " + navigator.cookieEnabled;
